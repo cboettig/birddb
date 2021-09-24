60f3d0a8ea658de459f88a755e2e3bbb732023b9
Matt Strimas-Mackey <mstrimas@gmail.com>
2021-09-24 11:51:44 -0400

    temporary fix for windows unit test error

--- tests/testthat/test-ebird.R
+++ tests/testthat/test-ebird.R
@@ -1,14 +1,14 @@
 test_that("birddb works", {
   temp_dir <- file.path(tempdir(), "birddb")
-  Sys.setenv("BIRDDB_HOME" = tempdir()) 
+  Sys.setenv("BIRDDB_HOME" = temp_dir) 
   Sys.setenv("BIRDDB_DUCKDB" = temp_dir) 
   
   import_ebird(sample_observation_data())
   import_ebird(sample_checklist_data())
   
-  
   # observations
-  observations <- observations()
+  con <- ebird_conn("observations")
+  observations <- observations(con)
   expect_s3_class(observations, "tbl")
   expect_s3_class(observations, "tbl_dbi")
   
@@ -16,9 +16,11 @@ test_that("birddb works", {
   expect_s3_class(out, "data.frame")
   expect_gt(nrow(out), 0)
   
+  DBI::dbDisconnect(con, shutdown = TRUE)
   
-  # checklists
-  checklists <- checklists()
+  # checklists 
+  con <- ebird_conn("checklists")
+  checklists <- checklists(con)
   expect_s3_class(checklists, "tbl")
   expect_s3_class(checklists, "tbl_dbi")
   
@@ -26,5 +28,7 @@ test_that("birddb works", {
   expect_s3_class(out, "data.frame")
   expect_gt(nrow(out), 0)
   
-  unlink(temp_dir)
+  DBI::dbDisconnect(con, shutdown = TRUE)
+  
+  unlink(temp_dir, recursive = TRUE)
 })
