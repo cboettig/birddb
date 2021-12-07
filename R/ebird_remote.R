d182bd2552ab3a5bde5555dc03d9b776d672beca
Carl <cboettig@gmail.com>
2021-12-07 22:17:57 +0000

    :bird:

--- R/ebird_remote.R
+++ R/ebird_remote.R
@@ -22,8 +22,14 @@ ebird_remote <-
             to_duckdb = FALSE,
             host = "minio.cirrus.carlboettiger.info",
             ...) {
-
-    server <- arrow::s3_bucket(bucket, endpoint_override = host)
+        
+    ## Not ideal, but these will cause problems if set    
+    unset_aws_env()
+        
+    server <- arrow::s3_bucket(bucket, 
+                               endpoint_override = host,
+                               ...)
+    
     path <- server$path(paste0(version, "/observations"))
     df <- arrow::open_dataset(path)
     if (to_duckdb) {
@@ -33,4 +39,18 @@ ebird_remote <-
         df <- arrow::to_duckdb(df)
     }
     df
+    }
+unset_aws_env <- function(){
+  
+  ## Consider re-setting these afterwards.
+  ## What about ~/.aws ?
+  ## Maybe set these to empty strings instead of unsetting?
+  
+  ## Would be nice if we could simply override the detection of these
+  Sys.unsetenv("AWS_DEFAULT_REGION")
+  Sys.unsetenv("AWS_S3_ENDPOINT")
+  Sys.unsetenv("AWS_ACCESS_KEY_ID")
+  Sys.unsetenv("AWS_SECRET_ACCESS_KEY")
 }
+
+
