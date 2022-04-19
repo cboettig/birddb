fea2029597026a54fdf9784b357cbf2a477576e8
Carl Boettiger <cobettig@gmail.com>
2022-04-19 23:22:40 +0000

    fix urls

--- inst/raw_download.R
+++ inst/raw_download.R
@@ -2,13 +2,16 @@ base <- "https://download.ebird.org/ebd/prepackaged/"
 snapshot <- "Mar-2022"
 checklists <- paste0(base, "ebd_sampling_rel", snapshot, ".tar") 
 observations <- paste0(base, "ebd_rel", snapshot, ".tar")
-raw <- file.path(ebird_data_dir(), "raw")
-
+raw <- file.path(birddb::ebird_data_dir(), "raw")
+dir.create(raw)
 
 download.file(checklists,
               file.path(raw,basename(checklists)),
-              method="wget")
+              method="wget", quiet=TRUE)
 download.file(observations,
               file.path(raw,basename(observations)),
-              method="wget")
+              method="wget", quiet=TRUE)
 
+
+
+base <- "https://minio.thelio.carlboettiger.info/ebird/"
